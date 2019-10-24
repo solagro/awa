4aa309968790c42980f3884e42f56d4650578b0d
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-10-25 00:02:44 +0200

    Load translations statically

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,15 @@
 import i18n from 'i18next';
+
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-import Backend from 'i18next-xhr-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import en from '../static/locales/en/translation.json';
+import de from '../static/locales/de/translation.json';
+import es from '../static/locales/es/translation.json';
+import et from '../static/locales/et/translation.json';
+import fr from '../static/locales/fr/translation.json';
 
 i18n
-  // load translation using xhr -> see /public/locales
-  // learn more: https://github.com/i18next/i18next-xhr-backend
-  .use(Backend)
-
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
@@ -19,6 +20,14 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    resources: {
+      en: { translation: en },
+      de: { translation: de },
+      es: { translation: es },
+      et: { translation: et },
+      fr: { translation: fr },
+    },
+
     fallbackLng: 'en',
     debug: (process.env.NODE_ENV === 'development'),
 
