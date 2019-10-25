935c5030f61400bdc33901b5733ecf69e61d443a
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-10-25 10:14:53 +0200

    Simplify home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,125 +1,27 @@
 import React from 'react';
 
+import { useTranslation } from 'react-i18next';
 
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Collapse from '@material-ui/core/Collapse';
 
-import StarIcon from '@material-ui/icons/Star';
-import InfoIcon from '@material-ui/icons/Info';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import Done from '@material-ui/icons/Done';
 import SEO from '../components/seo';
-import Image from '../components/image';
 import Layout from '../components/layout';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
-  nested: {
-    paddingLeft: theme.spacing(4),
-  },
-}));
+import i18n from '../i18n';
 
 const IndexPage = ({ pageContext }) => {
-  const classes = useStyles();
-  const [features, setFeatures] = React.useState(true);
-  const [info, setInfo] = React.useState(true);
+  const { t } = useTranslation();
 
-  function handleClick (id) {
-    switch (id) {
-      case 'features':
-        setFeatures(!features);
-        break;
-      case 'info':
-        setInfo(!info);
-        break;
-      default:
-    }
+  if (pageContext.language !== i18n.language) {
+    i18n.changeLanguage(pageContext.language);
   }
 
   return (
     <Layout>
       <SEO title="Home" />
-      <Grid container spacing={3} justify="center">
-        <Grid item xs={2}>
-          <div style={{ maxWidth: '100px', marginBottom: '1.45rem' }}>
-            <Image />
-          </div>
-        </Grid>
-        <Grid item xs={8}>
-          <h1>Gatsby Material UI Starter</h1>
-          <h5>
-            A responsive, minimalist Gatsby starter based on the world's most
-            popular React UI framework.
-          </h5>
-          <pre>{JSON.stringify(pageContext, null, 2)}</pre>
-        </Grid>
-      </Grid>
+      <h1>{t('pif')}</h1>
+      <pre>{JSON.stringify(pageContext, null, 2)}</pre>
       <Divider />
-      <List
-        component="nav"
-        className={classes.root}
-      >
-        <ListItem id="features" button onClick={() => handleClick('features')}>
-          <ListItemIcon>
-            <StarIcon />
-          </ListItemIcon>
-          <ListItemText primary="Features" />
-          {features ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
-        <Collapse in={!features} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon><Done /></ListItemIcon>
-              <ListItemText primary="Material UI Framework" />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon><Done /></ListItemIcon>
-              <ListItemText primary="Progressive Web App" />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon><Done /></ListItemIcon>
-              <ListItemText primary="SEO" />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon><Done /></ListItemIcon>
-              <ListItemText primary="Offline Support" />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon><Done /></ListItemIcon>
-              <ListItemText primary="Roboto Typeface (self hosted)" />
-            </ListItem>
-          </List>
-        </Collapse>
-        <ListItem button onClick={() => handleClick('info')}>
-          <ListItemIcon>
-            <InfoIcon />
-          </ListItemIcon>
-          <ListItemText primary="Info" />
-          {info ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
-        <Collapse in={!info} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon><Done /></ListItemIcon>
-              <ListItemText primary="Based on Gatsby Default Starter" />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon><Done /></ListItemIcon>
-              <ListItemText primary="Uses Gatsby Material UI Plugin" />
-            </ListItem>
-          </List>
-        </Collapse>
-      </List>
     </Layout>
   );
 };
