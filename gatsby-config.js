70f4aef9e2faf4eb93f3f7601f286638cbccb5f7
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-10-31 20:30:24 +0100

    Add Gatsby plugin for Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-netlify',
+    'gatsby-plugin-netlify-cms',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
