32b327605b40965472bfdf5a3a7d2e384ff5727e
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-10-31 14:16:41 +0100

    Remove temporary language switcher

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 
-import { useTranslation } from 'react-i18next';
-
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
-import LanguageSwitcherMenu from './LanguageSwitcherMenu';
 
 const useStyles = makeStyles({
   root: {
@@ -20,9 +15,6 @@ const Header = ({ siteTitle }) => {
   const classes = useStyles();
   const { i18n } = useTranslation();
 
-  const [showLanguageSwitcher, setShowLanguageSwitcher] = React.useState(false);
-  const { t } = useTranslation();
-
   return (
     <div className={classes.root}>
       <Typography variant="h5" component="div">
@@ -32,17 +24,6 @@ const Header = ({ siteTitle }) => {
       <Typography variant="h6" component="div">
         ({i18n.language})
       </Typography>
-
-      <Button
-        variant="contained"
-        onClick={() => setShowLanguageSwitcher(true)}
-      >
-        {t('Change language')}
-      </Button>
-      <LanguageSwitcherMenu
-        open={showLanguageSwitcher}
-        onClose={() => setShowLanguageSwitcher(false)}
-      />
     </div>
   );
 };
