db3f52ad3aeb4481b21591b8ad2c37e552afe763
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-11-05 12:24:56 +0100

    Setup gatsby-source-filesystem plugin to load quizz question files

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'quizz',
+        path: `${__dirname}/content/quizz`,
+      },
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
