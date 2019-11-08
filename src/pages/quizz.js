038e2181e883dec29408dce9ed0e610ce56e9975
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-11-08 01:30:47 +0100

    Add buttons to quizz themes on quizz main page

--- src/pages/quizz.js
+++ src/pages/quizz.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { useTranslation, Trans } from 'react-i18next';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import SEO from '../components/Seo';
+import Link from '../components/Link';
 import Layout from '../components/Layout';
 import doRedirect from '../hoc/doRedirect';
 
 const QuizzPage = () => {
   const { t, i18n } = useTranslation();
+
+  const { results: { questions } } = useStaticQuery(graphql`
+    query {
+      results: allQuizzJson {
+        questions: nodes {
+          theme
+        }
+      }
+    }
+  `);
+
+  const themes = questions.reduce((acc, { theme }) =>
+    (!acc.includes(theme) ? [...acc, theme] : acc), []);
 
   return (
     <Layout>
@@ -28,7 +44,16 @@ const QuizzPage = () => {
         </Trans>
       </Typography>
 
-      <pre>Quizz chooser</pre>
+      {themes.map(theme => (
+        <Button
+          key={theme}
+          component={Link}
+          to={`/quizz/${theme}`}
+          variant="outlined"
+        >
+          {theme}
+        </Button>
+      ))}
     </Layout>
   );
 };
