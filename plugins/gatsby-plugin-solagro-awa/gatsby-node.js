c738dcbe6c4e3f0264fd9606f74ba386c2c0589f
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-11-21 15:09:36 +0100

    Add description comments to localized page creation process

--- plugins/gatsby-plugin-solagro-awa/gatsby-node.js
+++ plugins/gatsby-plugin-solagro-awa/gatsby-node.js
@@ -5,9 +5,16 @@ const { generatePagesInfos } = require('./lib/generatePages.js');
  * Create main localized page
  */
 exports.onCreatePage = async ({ page, actions: { createPage }, store }) => {
+  /**
+   * Get locales from siteMetada through global state store as graphql querying
+   * is not available in onCreatePage
+   */
   const { config: { siteMetadata: { locales } } } = store.getState();
 
   if (page.isCreatedByStatefulCreatePages) {
+    /**
+     * Generate same page for each locale
+     */
     const i18nPages = generatePagesInfos(page, locales);
 
     // Create custom i18n pages
