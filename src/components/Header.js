cbd087601f8ddb58d754071fe0dafa3263481f6b
marianne lajante <marianne.lajante@makina-corpus.com>
2019-11-21 17:06:45 +0100

    Improve general style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,37 +3,43 @@ import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-  root: {
-    display: 'flex',
+  logo: {
+    width: 112,
   },
 });
 
-const Header = ({ siteTitle }) => {
+const Header = ({ siteTitle, parentSite, logo, preventDefault }) => {
   const classes = useStyles();
   const { i18n } = useTranslation();
 
   return (
-    <div className={classes.root}>
-      <Typography variant="h5" component="div">
-        {siteTitle}
-      </Typography>
+    <Grid container direction="row" justify="space-between">
+      <Link href={parentSite} onClick={preventDefault}>
+        <img className={classes.logo} src={logo} alt={siteTitle} />
+      </Link>
 
       <Typography variant="h6" component="div">
         ({i18n.language})
       </Typography>
-    </div>
+    </Grid>
   );
 };
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
+  parentSite: PropTypes.string,
+  logo: PropTypes.string,
 };
 
 Header.defaultProps = {
   siteTitle: '',
+  parentSite: '#',
+  logo: '',
 };
 
 export default Header;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,11 +14,12 @@ import { useTranslation } from 'react-i18next';
 import { Location } from '@reach/router';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Tooltip from '@material-ui/core/Tooltip';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import EmojiNatureIcon from '@material-ui/icons/EmojiNature';
 import Fab from '@material-ui/core/Fab';
-
+import Grid from '@material-ui/core/Grid';
 import Header from './Header';
 import './Layout.css';
 
@@ -27,11 +28,13 @@ import adaptPathname from '../lib/adaptPathname';
 
 const languageIds = Object.keys(locales);
 
-const drawerWidth = 80;
+const drawerWidth = 64;
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
+    alignContent: 'center',
+    background: 'url("/images/awa-background.svg") center -10% / cover no-repeat',
   },
   drawer: {
     width: drawerWidth,
@@ -65,51 +68,72 @@ const Layout = ({ children }) => {
 
   const classes = useStyles();
   const { t, i18n } = useTranslation();
-
+  const tableOfContent = [
+    { label: t('Home page'), path: '/' },
+    { label: t('Quiz'), path: '/quizz' },
+    { label: t('Observations'), path: '/map' },
+    { label: t('Adaptations'), path: '/adaptations' },
+  ];
   return (
-    <div style={{ minHeight: '100vh', backgroundColor: '#FFF' }} className={classes.root}>
-      <CssBaseline />
+    <Grid
+      container
+      className={classes.root}
+      justify="center"
+      alignItems="center"
+      alignContent="center"
+    >
+      <Grid item xs={12} md={9} xl={8}>
+        <CssBaseline />
 
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{ paper: classes.drawerPaper }}
-        anchor="left"
-      >
-        {['/', '/quizz', '/map', '/adaptations'].map(path => (
-          <Fab
-            color="primary"
-            aria-label="add"
-            className={classes.fab}
-            key={path}
-            to={`/${i18n.language}${path}`}
-            component={Link}
-          >
-            <EmojiNatureIcon />
-          </Fab>
-        ))}
-      </Drawer>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{ paper: classes.drawerPaper }}
+          anchor="left"
+        >
+          {tableOfContent.map(({ label, path }) => (
+            <Tooltip title={label} placement="right">
+              <Fab
+                color="primary"
+                aria-label="add"
+                className={classes.fab}
+                key={path}
+                to={`/${i18n.language}${path}`}
+                component={Link}
+                size="small"
+                boxShadow={10}
+              >
+                <EmojiNatureIcon />
+              </Fab>
+            </Tooltip>
+          ))}
+        </Drawer>
 
-      <div className={classes.content}>
-        <Header siteTitle={siteMetadata.title} />
-        <main>{children}</main>
+        <div className={classes.content}>
+          <Header
+            siteTitle={siteMetadata.title}
+            parentSite="https://agriadapt.eu/"
+            logo="/images/agriAdapt-logo.svg"
+          />
+          <main>{children}</main>
 
-        <footer style={{ paddingTop: 10 }}>
-          {t('Last build')} {buildTime}
-          <br />
-          <Location>
-            {({ location: { pathname } }) => (
-              languageIds.map(language => (
-                <span key={language}>
-                  <Link to={adaptPathname(pathname, language)} lang={language}>{language}</Link>
-                  {' '}
-                </span>
-              ))
-            )}
-          </Location>
-        </footer>
-      </div>
-    </div>
+          <footer style={{ paddingTop: 10 }}>
+            {t('Last build')} {buildTime}
+            <br />
+            <Location>
+              {({ location: { pathname } }) => (
+                languageIds.map(language => (
+                  <span key={language}>
+                    <Link to={adaptPathname(pathname, language)} lang={language}>{language}</Link>
+                    {' '}
+                  </span>
+                ))
+              )}
+            </Location>
+          </footer>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
