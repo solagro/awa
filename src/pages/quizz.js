5af9d2b5a00076b78f7ea4930c8987fcb0583739
marianne lajante <marianne.lajante@makina-corpus.com>
2019-11-22 15:03:21 +0100

    Style quiz zone page

--- src/pages/quizz.js
+++ src/pages/quizz.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import { useTranslation, Trans } from 'react-i18next';
 import { useStaticQuery, graphql } from 'gatsby';
+import clsx from 'clsx';
 
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Grid from '@material-ui/core/Grid';
+import Tooltip from '@material-ui/core/Tooltip';
+import { makeStyles } from '@material-ui/core/styles';
 
 import SEO from '../components/Seo';
 import Link from '../components/Link';
 import Layout from '../components/Layout';
 import doRedirect from '../hoc/doRedirect';
 
+
+const useStyles = makeStyles(theme => ({
+  module: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingBottom: theme.spacing(10),
+  },
+  module__title: {
+    justifyContent: 'center',
+  },
+  category__title: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  themes__group: {
+    marginTop: theme.spacing(4),
+  },
+  theme__button: {
+    padding: '0px 48px',
+  },
+  theme__button_image: {
+    maxHeight: 200,
+  },
+  atlantique: {
+    minWidth: '50%',
+    order: 2,
+    textAlign: 'right',
+    alignSelf: 'center',
+  },
+  nord: {
+    minWidth: '100%',
+    margin: 'auto',
+    order: 1,
+    textAlign: 'center',
+    alignSelf: 'center',
+  },
+  common: {
+    minWidth: '50%',
+    order: 2,
+    textAlign: 'left',
+    alignSelf: 'center',
+  },
+}));
+
 const QuizzPage = ({ location: { pathname } }) => {
   const { t, i18n } = useTranslation();
+  const classes = useStyles();
 
   const { results: { questions = [] } = {} } = useStaticQuery(graphql`
     {
@@ -40,31 +91,54 @@ const QuizzPage = ({ location: { pathname } }) => {
   return (
     <Layout>
       <SEO title={t('Quizz')} lang={i18n.language} />
+      <Grid container justify="center" className={classes.module}>
 
-      <Typography variant="h1" gutterBottom>
-        {t('Main title of quizz page')}
-      </Typography>
+        <Typography className={classes.module__title} variant="h1" gutterBottom>
+          {t('Farm vulnerability and adaptation Quiz')}
+        </Typography>
 
-      <Typography variant="h2" gutterBottom>
-        {t('Subtitle of quizz page')}
-      </Typography>
+        <Typography className={classes.category__title} variant="h2" gutterBottom>
+          {t('zone selection')}
+        </Typography>
 
-      <Typography variant="body1">
-        <Trans>
-          Introduction or description of quizz initial choice
-        </Trans>
-      </Typography>
-
-      {themes.map(theme => (
-        <Button
-          key={theme}
-          component={Link}
-          to={`${pathname}/${theme}/${firstQuestions[theme]}`}
-          variant="outlined"
+        <Typography variant="subtitle1" gutterBottom>
+          <Trans>
+        Please select the area in which you would like to test your knowledge
+          </Trans>
+        </Typography>
+        <Grid
+          container
+          className={classes.themes__group}
+          justify="center"
+          wrap="wrap"
         >
-          {theme}
-        </Button>
-      ))}
+          {themes.map(theme => (
+            <Grid
+              item
+              className={clsx({
+                [classes.theme__button]: true,
+                [classes[theme]]: theme,
+              })}
+              xs={12}
+              md={6}
+              xl={4}
+            >
+              <Tooltip
+                title={t(`${theme} region. Available languages: English standard or ${i18n.language} by changing the language of the site`)}
+                placement="left-start"
+              >
+                <ButtonBase
+                  key={theme}
+                  component={Link}
+                  to={`${pathname}/${theme}/${firstQuestions[theme]}`}
+                >
+                  <img className={classes.theme__button_image} src={`../../images/themes/${theme}.png`} alt={theme} />
+                </ButtonBase>
+              </Tooltip>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
