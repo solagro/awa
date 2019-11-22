c6534be4056587cfc49fb90d1fac0b26c9ef889c
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-11-22 15:12:21 +0100

    Generate page for each grid point

--- plugins/gatsby-plugin-solagro-awa-map/gatsby-node.js
+++ plugins/gatsby-plugin-solagro-awa-map/gatsby-node.js
@@ -1,5 +1,8 @@
+const path = require('path');
 const csvtojson = require('csvtojson');
 const camelcase = require('camelcase');
+
+const REPORTER_PREFIX = '[solagro-awa-quizz-map] ';
 
 const { removeNumberPrefix } = require('./lib/helpers');
 
@@ -49,11 +52,80 @@ exports.onCreateNode = async ({
   }
 };
 
-exports.createPages = async () => {
+exports.createPages = async ({ reporter, graphql, actions: { createPage } }) => {
+  const { data: { site: { siteMetadata: { locales } } } } = await graphql(`
+    query {
+      site {
+        siteMetadata {
+          locales
+        }
+      }
+    }
+  `);
+
   /**
-   * Create page for each grid code
+   * Get all available grid codes from raw data
    */
-  // TODO
+  const { data } = await graphql(`
+    query {
+      allYieldCompilation {
+        group(field: gridCode) {
+          gridCode: fieldValue
+        }
+      }
+
+      allClimateObservation {
+        group(field: gridCode) {
+          gridCode: fieldValue
+        }
+      }
+
+      allClimateProjections {
+        group(field: gridCode) {
+          gridCode: fieldValue
+        }
+      }
+    }
+  `);
+
+  /**
+   * Extract all grid codes from query result
+   */
+  const duplicatedGridCodes = Object.values(data)
+    .reduce((acc, curr) => ([
+      ...acc,
+      ...curr.group.map(({ gridCode }) => gridCode),
+    ]), []);
+
+  /**
+   * Dedup grid codes
+   */
+  const gridCodes = Array.from(new Set(duplicatedGridCodes));
+
+  /**
+   * Output debug message to console when building
+   */
+  if (!gridCodes.length) {
+    reporter.warn(`${REPORTER_PREFIX}No grid codes found.`);
+  } else {
+    reporter.info(`${REPORTER_PREFIX}${gridCodes.length} grid code found.`);
+  }
+
+  /**
+   * Create page for each grid point:
+   *  /{lng}/map/{gridCode}
+   */
+  await Promise.all(locales.map(language =>
+    Promise.all(gridCodes.map(gridCode => createPage({
+      path: `/${language}/map/${gridCode}`,
+      component: path.resolve('./src/components/GridCode.js'),
+      context: {
+        language,
+        gridCode,
+      },
+    })))));
+
+  reporter.info(`${REPORTER_PREFIX}${locales.length * gridCodes.length} grid point pages created.`);
 };
 
 /**
---
+++ src/components/GridCode.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { graphql } from 'gatsby';
+
+import Typography from '@material-ui/core/Typography';
+import SEO from './Seo';
+import Layout from './Layout';
+
+import doRedirect from '../hoc/doRedirect';
+
+const GridCode = ({
+  pageContext: { gridCode },
+  data,
+}) => {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <Layout>
+      <SEO title={t('Active site detailed information card')} lang={i18n.language} />
+
+      <Typography variant="h1" gutterBottom>
+        {t('Active site detailed information card')}
+      </Typography>
+
+      <Typography variant="h3" component="h2">
+        {t('Question')}
+      </Typography>
+
+      <pre>
+        {JSON.stringify({
+          gridCode,
+          data,
+        }, null, 2)}
+      </pre>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query ($gridCode: String) {
+    allClimateObservation(filter: {gridCode: {eq: $gridCode}}) {
+      nodes {
+        year
+        category
+      }
+    }
+  }
+`;
+
+export default doRedirect(GridCode);
