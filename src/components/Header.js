bea0bbf68ee7b1b4e9000c245465eb8eea0f1089
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-11-26 00:20:30 +0100

    Use right link for language selector

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 
+import { Link } from 'gatsby';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Location } from '@reach/router';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
 import adaptPathname from '../lib/adaptPathname';
