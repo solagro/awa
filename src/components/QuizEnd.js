815006e12e7ecdf9ca78c67cf142952bea1e880e
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-11-26 23:23:11 +0100

    Remove useless import from QuizEnd page

--- src/components/QuizEnd.js
+++ src/components/QuizEnd.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { graphql } from 'gatsby';
-import clsx from 'clsx';
 
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
-import Done from '@material-ui/icons/Done';
-import Clear from '@material-ui/icons/Clear';
 import { makeStyles } from '@material-ui/core/styles';
 
-import QuizButton from './QuizButton';
 import SEO from './Seo';
 import Layout from './Layout';
 import Link from './Link';
