cf6d7d65f4c1de000f1383a5d97629f6339d82b9
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-11-26 01:24:47 +0100

    Add horizontal scroll to grid point data tables

--- src/components/CustomDataTable.js
+++ src/components/CustomDataTable.js
@@ -7,29 +7,40 @@ import TableBody from '@material-ui/core/TableBody';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: '100%',
+    overflow: 'auto',
+  },
+});
 
 const CustomDataTable = ({ keys = [], years = [] }) => {
   const { t } = useTranslation();
+  const classes = useStyles();
 
   return (
-    <Table size="small">
-      <TableHead>
-        <TableRow>
-          <TableCell>{t('Year')}</TableCell>
-          {keys.map(({ fieldValue: key }) =>
-            <TableCell key={key}>{key}</TableCell>)}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {years.map(({ fieldValue: year, nodes }) => (
-          <TableRow key={year} hover>
-            <TableCell component="th" scope="row">{year}</TableCell>
-            {nodes.map(({ value }, index) =>
-              <TableCell key={index}>{value}</TableCell>)}
+    <div className={classes.root}>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>{t('Year')}</TableCell>
+            {keys.map(({ fieldValue: key }) =>
+              <TableCell key={key}>{key}</TableCell>)}
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHead>
+        <TableBody>
+          {years.map(({ fieldValue: year, nodes }) => (
+            <TableRow key={year} hover>
+              <TableCell component="th" scope="row">{year}</TableCell>
+              {nodes.map(({ value }, index) =>
+                <TableCell key={index}>{value}</TableCell>)}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 };
 
