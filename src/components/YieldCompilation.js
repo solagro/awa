6e8f3a6faaefe77d53eaff42565db520e3ff0829
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-12-06 19:07:31 +0100

    Colorize YieldCompilation data table according quartiles

--- src/components/YieldCompilation.js
+++ src/components/YieldCompilation.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { graphql } from 'gatsby';
 
+import TableCell from '@material-ui/core/TableCell';
+import { makeStyles } from '@material-ui/core';
+
 import CustomDataTable from './CustomDataTable';
 import GridPointTabs from './GridPointTabs';
 import Layout from './Layout';
 import SEO from './Seo';
 
 import doRedirect from '../hoc/doRedirect';
+import {
+  getQuartiles,
+  getQuantileGroup,
+} from '../lib/math';
+
+const useStyles = makeStyles({
+  quart1: { background: '#97d492' },
+  quart2: { background: '#c6d799' },
+  quart3: { background: '#dac5a0' },
+  quart4: { background: '#dea8a8' },
+});
 
 const YieldCompilation = ({
   pageContext: { sourceType, gridCode },
@@ -19,6 +33,7 @@ const YieldCompilation = ({
   },
 }) => {
   const { t, i18n } = useTranslation();
+  const classes = useStyles();
 
   return (
     <Layout>
@@ -29,6 +44,17 @@ const YieldCompilation = ({
       <CustomDataTable
         keys={allKeys}
         years={allYears}
+        renderCell={({ key, value, col }) => {
+          const quartiles = getQuartiles(col);
+          const groupe = getQuantileGroup(value, quartiles);
+          const className = classes[`quart${groupe}`];
+
+          return (
+            <TableCell key={key} className={className}>
+              {value}
+            </TableCell>
+          );
+        }}
       />
     </Layout>
   );
---
+++ src/lib/math.js
@@ -0,0 +1,68 @@
+
+/**
+ * Get the percentile value of given array of numbers
+ *
+ * @param {number[]} values An array of the values to group
+ * @param {number} [percentile=50] A percentile
+ * @returns {number} Percentile value
+ */
+export const getQuantile = (values, percentile = 50) => {
+  const sortedValues = [...values].sort((a, b) => (a - b));
+  const index = percentile / 100.0 * (values.length - 1);
+
+  if (Math.floor(index) === index) {
+    return sortedValues[index];
+  }
+
+  const i = Math.floor(index);
+  const fraction = index - i;
+  return sortedValues[i] + (sortedValues[i + 1] - sortedValues[i]) * fraction;
+};
+
+/**
+ * Get the percentile values of given array of numbers
+ *
+ * @param {number[]} values An array of the values to group
+ * @param {number[]} [percentile=50] An array of percentiles
+ * @returns {number[]} Array of percentile values
+ */
+export const getQuantiles =  (values, percentile = []) =>
+  percentile.map(percent => getQuantile(values, percent));
+
+export const getMedian = values =>
+  getQuantiles(values, 50);
+
+export const getQuartiles = values =>
+  getQuantiles(values, [25, 50, 75]);
+
+export const getDeciles = values =>
+  getQuantiles(values, [10, 20, 30, 40, 50, 60, 70, 80, 90]);
+
+export const getCentiles = values =>
+  getQuantiles(values, [...Array(99)].map((v, i) => i + 1));
+
+/**
+ * Get the group owning a number, given an array of percentiles
+ *
+ * @param {number} value
+ * @param {number[]} [quantiles=[]]
+ * @returns {number} The who's ownging the value
+ */
+export const getQuantileGroup = (value, quantiles = []) => {
+  if (value >= [...quantiles].pop()) {
+    return quantiles.length + 1;
+  }
+
+  return quantiles.findIndex(qt => (value < qt)) + 1;
+};
+
+export default {
+  getQuantile,
+  getQuantiles,
+  getQuantileGroup,
+
+  getMedian,
+  getQuartiles,
+  getDeciles,
+  getCentiles,
+};
