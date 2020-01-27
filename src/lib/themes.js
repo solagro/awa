bf5a7dfd2a722397037dc984081cecd50e746226
marianne lajante <marianne.lajante@makina-corpus.com>
2020-01-27 16:45:25 +0100

    Adapt tooltip text

---
+++ src/lib/themes.js
@@ -0,0 +1,20 @@
+const themes = {
+  atlantic: {
+    mainCountry: 'France',
+    mainLanguage: 'french',
+  },
+  continental: {
+    mainCountry: 'Germany',
+    mainLanguage: 'german',
+  },
+  meridional: {
+    mainCountry: 'Spain',
+    mainLanguage: 'spanish',
+  },
+  north: {
+    mainCountry: 'Estonia',
+    mainLanguage: 'estonian',
+  },
+};
+
+export default themes;
