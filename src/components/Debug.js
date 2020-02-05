746df469d561d9c979575e04649393bab595abed
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-02-05 19:09:21 +0100

    Improve Debug component

--- src/components/Debug.js
+++ src/components/Debug.js
@@ -2,9 +2,13 @@ import React from 'react';
 import doRedirect from '../hoc/doRedirect';
 
 const Debug = props => (
-  <pre>
-    {JSON.stringify(props, null, 2)}
-  </pre>
+  Object.entries(props).map(([propName, propValue]) => (
+    <pre key={propName}>
+      {propName}
+      <hr />
+      {JSON.stringify(propValue, null, 2)}
+    </pre>
+  ))
 );
 
 export default doRedirect(Debug);
