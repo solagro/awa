a83fd0c5cbf842bd4dee3c520cf26365690d06dc
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-02-18 02:04:32 +0100

    Allow MarkdownText to be extended

--- src/components/MarkdownText.js
+++ src/components/MarkdownText.js
@@ -4,19 +4,22 @@ import Rehype2react from 'rehype-react';
 
 import Typography from '@material-ui/core/Typography';
 
-const renderAst = new Rehype2react({
-  createElement: React.createElement,
-  components: {
-    h1: props => <Typography variant="h1" {...props} />,
-    h2: props => <Typography variant="h2" {...props} />,
-    h3: props => <Typography variant="h3" {...props} />,
-    h4: props => <Typography variant="h4" {...props} />,
-    h5: props => <Typography variant="h5" {...props} />,
-    h6: props => <Typography variant="h6" {...props} />,
-    p: props => <Typography variant="body2" {...props} />,
-  },
-}).Compiler;
+const MarkdownText = ({ hast, components }) => {
+  const renderAst = new Rehype2react({
+    createElement: React.createElement,
+    components: {
+      h1: props => <Typography variant="h1" paragraph {...props} />,
+      h2: props => <Typography variant="h2" paragraph {...props} />,
+      h3: props => <Typography variant="h3" paragraph {...props} />,
+      h4: props => <Typography variant="h4" paragraph {...props} />,
+      h5: props => <Typography variant="h5" paragraph {...props} />,
+      h6: props => <Typography variant="h6" paragraph {...props} />,
+      p: props => <Typography variant="body2" paragraph {...props} />,
+      ...components,
+    },
+  }).Compiler;
 
-const MarkdownText = ({ hast }) => renderAst(hast);
+  return renderAst(hast);
+};
 
 export default MarkdownText;
