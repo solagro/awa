bf666882f4fd5f5d9e5ce41308afd4e492e7b0c9
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-02-25 01:55:53 +0100

    Remove previous GraphQL nodes creation

--- plugins/gatsby-transformer-quiz-markdown/gatsby-node.js
+++ plugins/gatsby-transformer-quiz-markdown/gatsby-node.js
@@ -1,8 +1,5 @@
 const path = require('path');
 const slugify = require('slugify');
-const camelcase = require('camelcase');
-
-const capitalize = value => camelcase(value, { pascalCase: true });
 
 /**
  * Transform JSON nodes created through gatsby-transformer-json
@@ -56,57 +53,5 @@ exports.onCreateNode = async ({
         },
       });
     });
-
-    await Promise.all(['question', 'explanation', 'learn-more'].map(async element => {
-      const content = node[element];
-
-      if (content) {
-        const id = `${node.id}-Markdown${capitalize(element)}En`;
-
-        await createNode({
-          id,
-          parent: node.id,
-          dir: path.resolve('./'),
-          internal: {
-            type: `${node.internal.type}Markdown${capitalize(element)}En`,
-            mediaType: 'text/markdown',
-            content,
-            contentDigest: createContentDigest(content),
-          },
-        });
-
-        // Create relation between source node created node
-        await createNodeField({
-          node,
-          name: `markdown${capitalize(element)}En___NODE`,
-          value: id,
-        });
-      }
-
-      if (node[`${element}-i18n`] && node[`${element}-i18n`].length) {
-        await Promise.all(node[`${element}-i18n`].map(async ({ language, [element]: contentInt }) => {
-          const id = `${node.id}-Markdown${capitalize(element)}${capitalize(language)}`;
-
-          await createNode({
-            id,
-            parent: node.id,
-            dir: path.resolve('./'),
-            internal: {
-              type: `${node.internal.type}Markdown${capitalize(element)}${capitalize(language)}`,
-              mediaType: 'text/markdown',
-              content: contentInt,
-              contentDigest: createContentDigest(contentInt),
-            },
-          });
-
-          // Create relation between source node created node
-          await createNodeField({
-            node,
-            name: `markdown${capitalize(element)}${capitalize(language)}___NODE`,
-            value: id,
-          });
-        }));
-      }
-    }));
   }
 };
