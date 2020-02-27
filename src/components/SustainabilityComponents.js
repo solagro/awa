8565dc43fced9a209c766d6b686dae225bf49d35
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-02-27 12:24:07 +0100

    Use sustainability components impacts from catalog
    	Also add tooltip

--- src/components/SustainabilityComponents.js
+++ src/components/SustainabilityComponents.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { graphql, useStaticQuery } from 'gatsby';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -22,15 +23,28 @@ const sustainabilityComponents = [
 const SustainabilityComponents = ({ measure }) => {
   const { t } = useTranslation();
 
+  const { catalog } = useStaticQuery(graphql`
+    query {
+      catalog: adaptationsJson {
+        impacts: sustainability_impact {
+          id
+          value
+        }
+      }
+    }
+  `);
+
+  const impacts = catalog.impacts.reduce((store, { id, value }) => ({ ...store, [id]: value }), {});
+
   return (
     <List dense>
       {sustainabilityComponents.map(sustainabilityComponent => (
-        <Tooltip
-          key={sustainabilityComponent}
-          title={t(`${sustainabilityComponent}-explanation`)}
-          arrow
-        >
-          <ListItem>
+        <ListItem key={sustainabilityComponent}>
+          <Tooltip
+            title={t(impacts[measure[sustainabilityComponent]])}
+            placement="left"
+            arrow
+          >
             <ListItemIcon>
               <img
                 src={`/images/sustainability/impact-${measure[sustainabilityComponent]}.png`}
@@ -38,11 +52,13 @@ const SustainabilityComponents = ({ measure }) => {
                 alt=""
               />
             </ListItemIcon>
+          </Tooltip>
+          <Tooltip title={t(`${sustainabilityComponent}-explanation`)}>
             <ListItemText>
               {t(sustainabilityComponent)}
             </ListItemText>
-          </ListItem>
-        </Tooltip>
+          </Tooltip>
+        </ListItem>
       ))}
     </List>
   );
