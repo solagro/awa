820fc501cf591d68f71936201c91f793fbde405e
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-02-27 16:15:17 +0100

    Hide language link list from redirection page

--- src/components/NoScript.js
+++ src/components/NoScript.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { Link as GatsbyLink } from 'gatsby';
+
 import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
 
 import locales from '../locales';
 import adaptPathname from '../lib/adaptPathname';
+
+const useStyles = makeStyles({
+  list: {
+    listStyle: 'none',
+
+    height: '100vh',
+    margin: 0,
+    padding: 0,
+
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    animation: '$fadein 2s',
+  },
+  '@keyframes fadein': {
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  },
+  listItem: {
+    margin: 0,
+    padding: 0,
+  },
+});
 
 const languages = Object.keys(locales);
 const languageNames = {
@@ -14,16 +41,20 @@ const languageNames = {
   es: 'Spanish',
 };
 
-const NoScript = () => (
-  <ul>
-    {languages.map(language => (
-      <li key={language}>
-        <Link component={GatsbyLink} to={adaptPathname('/', language)}>
-          {`Browse the site in ${languageNames[language]}`}
-        </Link>
-      </li>
-    ))}
-  </ul>
-);
+const NoScript = () => {
+  const classes = useStyles();
+
+  return (
+    <ul className={classes.list}>
+      {languages.map(language => (
+        <li key={language} className={classes.listItem}>
+          <Link component={GatsbyLink} to={adaptPathname('/', language)}>
+            {`Browse the site in ${languageNames[language]}`}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default NoScript;
