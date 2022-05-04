dd7116fd1dc870f55df37bcd0c73a43ad216047f
Romain Boissieres <romainboissieres@makina-corpus.com>
2022-05-04 16:06:47 +0200

    footer back to rules reglementation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,14 +31,6 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     alignItems: 'center',
   },
-  footer__partnership_grid: {
-    [theme.breakpoints.down('lg')]: {
-      flexWrap: 'wrap',
-    },
-    [theme.breakpoints.up('lg')]: {
-      flexWrap: 'nowrap',
-    },
-  },
   footer__title: {
     marginTop: theme.spacing(6),
   },
@@ -57,85 +49,84 @@ const Footer = () => {
       className={classes.footer}
       spacing={3}
     >
-      <Grid item xs={12} md={12} spacing={10}>
-        <Typography variant="h2" gutterBottom>{t('Partnership and contacts')}</Typography>
-      </Grid>
-      <Grid item xs={12} lg={12} className={classes.footer__logo}>
-        <Typography variant="body1" paragraph>
-          info@agriadapt.eu
-        </Typography>
-      </Grid>
-      <Grid
-        container
-        wrap="nowrap"
-        className={classes.footer__partnership_grid}
-        spacing={3}
-        justify="flex-start"
-        alignItems="baseline"
-      >
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <a target="_blank" rel="noopener noreferrer nofollow" href="https://ec.europa.eu/easme/en/life">
-            <img className={classes.footer__logo} src="/images/logos/Life.jpg" alt="life logo" />
-          </a>
-          <Typography variant="body1" paragraph>
-            {t('With the contribution of the LIFE financial instrument of the European Union')}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <a target="_blank" rel="noopener noreferrer nofollow" href="http://www.bodensee-stiftung.org">
-            <img src="/images/logos/LCF.jpg" alt="LCF logo" />
-          </a>
-          <Typography variant="h3">
-            Bodensee- Stiftung (Lake Constance Foundation)
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Patrick Trötschler : patrick.troetschler@bodensee-stiftung.org
-            <Typography variant="body1" component="br" />
-            Carolina Wackerhagen : Carolina.wackerhagen@bodensee-stiftung.org
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <a target="_blank" rel="noopener noreferrer nofollow" href="https://www.emu.ee/en">
-            <img src="/images/logos/EMU.jpg" alt="EMU logo" />
-          </a>
-          <Typography variant="h3">
-            Eesti Maaülikool (Estonian University of Life Sciences)
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Ragnar Leming : Ragnar.Leming@emu.ee
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <a target="_blank" rel="noopener noreferrer nofollow" href="https://fundacionglobalnature.org">
-            <img src="/images/logos/FGN.jpg" alt="FGN logo" />
-          </a>
-          <Typography variant="h3">
-            Fundación Global Nature
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Vanessa Sánchez : vsanchez@fundacionglobalnature.org
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <a target="_blank" rel="noopener noreferrer nofollow" href="https://solagro.org">
-            <img src="/images/logos/Solagro.jpg" alt="Solagro logo" />
-          </a>
-          <Typography variant="h3">
-            Solagro
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Nicolas Métayer : Nicolas.metayer@solagro.asso.fr
-          </Typography>
+      <Grid item xs={12} md={12}>
+        <Grid container spacing={2}>
+          <Grid item xs={6} lg={2} className={classes.footer__partnership_life}>
+            <a target="_blank" rel="noopener noreferrer nofollow" href="https://ec.europa.eu/easme/en/life">
+              <img className={classes.footer__logo} src="/images/logos/Life.jpg" alt="life logo" />
+            </a>
+          </Grid>
+          <Grid item xs={6} lg={4} className={classes.footer__partnership_text}>
+            <Typography variant="body1" paragraph>
+              {t('With the contribution of the LIFE financial instrument of the European Union')}
+            </Typography>
+          </Grid>
         </Grid>
       </Grid>
 
-      <Grid item xs={12} md={12} lg={12} className={classes.footer__logo}>
-        <Typography variant="body1" paragraph>
-          {t('Funding for the revision of the AWA Webtool within the framework')}
-        </Typography>
-        <Link to="/legal">
-          <Typography variant="h2" gutterBottom className={classes.footer__title}>{t('Legal')}</Typography>
-        </Link>
+      <Grid item xs={12} md={12}>
+        <Typography variant="h2" gutterBottom>{t('Partnership and contacts')}</Typography>
+        <Grid container spacing={6} className={classes.footer__partnership}>
+          <Grid item xs={12} lg={12} className={classes.footer__logo}>
+            <Typography variant="body1" paragraph>
+              info@agriadapt.eu
+            </Typography>
+          </Grid>
+          <Grid item xs={6} lg={3} className={classes.footer__logo}>
+            <a target="_blank" rel="noopener noreferrer nofollow" href="http://www.bodensee-stiftung.org">
+              <img src="/images/logos/LCF.jpg" alt="LCF logo" />
+            </a>
+            <Typography variant="h3">
+              Bodensee- Stiftung (Lake Constance Foundation)
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Patrick Trötschler : patrick.troetschler@bodensee-stiftung.org
+              <Typography variant="body1" component="br" />
+              Carolina Wackerhagen : Carolina.wackerhagen@bodensee-stiftung.org
+            </Typography>
+          </Grid>
+          <Grid item xs={6} lg={3} className={classes.footer__logo}>
+            <a target="_blank" rel="noopener noreferrer nofollow" href="https://www.emu.ee/en">
+              <img src="/images/logos/EMU.jpg" alt="EMU logo" />
+            </a>
+            <Typography variant="h3">
+              Eesti Maaülikool (Estonian University of Life Sciences)
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Ragnar Leming : Ragnar.Leming@emu.ee
+            </Typography>
+          </Grid>
+          <Grid item xs={6} lg={3} className={classes.footer__logo}>
+            <a target="_blank" rel="noopener noreferrer nofollow" href="https://fundacionglobalnature.org">
+              <img src="/images/logos/FGN.jpg" alt="FGN logo" />
+            </a>
+            <Typography variant="h3">
+              Fundación Global Nature
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Vanessa Sánchez : vsanchez@fundacionglobalnature.org
+            </Typography>
+          </Grid>
+          <Grid item xs={6} lg={3} className={classes.footer__logo}>
+            <a target="_blank" rel="noopener noreferrer nofollow" href="https://solagro.org">
+              <img src="/images/logos/Solagro.jpg" alt="Solagro logo" />
+            </a>
+            <Typography variant="h3">
+              Solagro
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Nicolas Métayer : Nicolas.metayer@solagro.asso.fr
+            </Typography>
+          </Grid>
+          <Grid item xs={12} lg={11} className={classes.footer__logo}>
+            <Typography variant="body1" paragraph>
+                {t('Funding for the revision of the AWA Webtool within the framework')}
+            </Typography>
+            <Link to="/legal">
+              <Typography variant="h2" gutterBottom className={classes.footer__title}>{t('Legal')}</Typography>
+            </Link>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
